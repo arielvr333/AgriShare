5938600c50bacb3ec1c9e637789d71c1e22aee3b
Ariel Verbitsky <arielvr333@gmail.com>
2022-03-07 22:47:18 +0200

    room fix+ intro login + ui change

---
+++ app/src/main/java/com/example/agrishare/IntroActivity.java
@@ -0,0 +1,51 @@
+package com.example.agrishare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.agrishare.feed.MainActivity;
+import com.example.agrishare.login.LoginActivity;
+import com.example.agrishare.model.Model;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Model.instance.executor.execute(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (Model.instance.isSignedIn()){
+                Model.instance.mainThread.post(() -> {
+                    toFeedActivity();
+                });
+            }else{
+                Model.instance.mainThread.post(() -> {
+                    toLoginActivity();
+                });
+            }
+        });
+
+    }
+
+    private void toLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void toFeedActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
